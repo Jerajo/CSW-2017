12ced854cc28130a8721de0da22bbd08adbe1202
Jerajo <jessejose@outlook.es>
2017-06-22 14:45:10 -0400

    sincronisado

--- Calculadora_Standar_Windows/Form1.cs
+++ Calculadora_Standar_Windows/Form1.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
+// repositorio de github
 namespace Calculadora_Standar_Windows
 {
     public partial class Form1 : Form
